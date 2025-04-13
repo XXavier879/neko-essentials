9d942fff7bec838ea0d3134570754523117ff0c9
XXavier879 <xavier.rojas.viafara879@gmail.com>
2025-04-13 01:37:04 -0400

    Update 1.0.15

--- beh/scripts/chathandler.js
+++ beh/scripts/chathandler.js
@@ -621,7 +621,7 @@ commandManager.registerCommand(
   'Show information about the plugin',
   (context) => {
     const player = context.getSource();
-    player.sendMessage("Neko Commander\nV: 1.0.14\nRelease: false\nopensource: true");
+    player.sendMessage("Neko Commander\nV: 1.0.15\nRelease: false\nopensource: true");
     logger.info(`[commandManager] ${player.name} accessed info`);
   },
   [],
